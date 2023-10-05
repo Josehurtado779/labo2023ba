6c0fca5abd3d367ca30b025dd636bc258e0a836a
Josehurtado779 <142694945+Josehurtado779@users.noreply.github.com>
2023-10-05 06:51:05 +0000

    mar

--- src/WORKFLOW-ARRECHOYMAS/601_RUN_correr_workflow_002.r
+++ src/WORKFLOW-ARRECHOYMAS/601_RUN_correr_workflow_002.r
@@ -1,12 +1,20 @@
+# Corrida general del workflow
+
 options(error = function() {
   traceback(20)
   options(error = NULL)
   stop("exiting after script error")
 })
 
-source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_002.r")
-source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_002.r")
-source("~/labo2023ba/src/workflow-inicial/631_FE_historia_002.r")
-source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_002.r")
-source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_002.r")
-source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_002.r")
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/labo2023ba/src/WORKFLOW_ARRECHOYMAS/611_CA_reparar_dataset_002.r")
+source("~/labo2023ba/src/WORKFLOW_ARRECHOYMAS/z621_DR_corregir_drifting_002.r")
+source("~/labo2023ba/src/WORKFLOW_ARRECHOYMAS/z631_FE_historia_002.r")
+source("~/labo2023ba/src/WORKFLOW_ARRECHOYMAS/z641_TS_training_strategy_002.r")
+
+# ultimos pasos, muy lentos
+source("~/labo2023ba/src/WORKFLOW_ARRECHOYMAS/z651_HT_lightgbm_002.r")
+source("~/labo2023ba/src/WORKFLOW_ARRECHOYMAS/z661_ZZ_final_002.r")
